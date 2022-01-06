526e237184d8e73a93a9aee51228131e8c62a711
Dimitri VINET <dimitri.vinet@edu.devinci.fr>
2022-01-06 14:02:23 +0100

    advancement

--- src/libalfred/wrapper/alfredapi.py
+++ src/libalfred/wrapper/alfredapi.py
@@ -2,6 +2,9 @@ import logging
 from typing import Optional
 
 from libalfred.wrapper.mixins.redis_user import RedisUserMixin
+from libalfred.utils.position import Position
+from libalfred.utils.command import Command
+from libalfred.wrapper import exceptions
 
 logger = logging.getLogger(__name__)
 
@@ -11,18 +14,57 @@ class AlfredAPI(RedisUserMixin):
 
     host: str
     port: int
-    password: str
+    password: Optional[str]
+    pub_channel: str
 
     def __init__(
         self,
         host: str,
         port: int,
-        password: Optional[str] = None,
+        password: str = None,
+        pub_channel: str = "command-robot",
         auto_connect: bool = False,
     ):
         self.host = host
         self.port = port
         self.password = password
+        self.pub_channel = pub_channel
 
         if auto_connect:
             self.connect_redis(self.host, self.port, self.password)
+
+    def publish_command(self, command: Command):
+        if not self.connected:
+            raise exceptions.NotConnectedException(
+                "This AlfredAPI instance is not connected to a redis server."
+            )
+        self.rc.publish(self.pub_channel, repr(command))
+
+    def move_line(
+        self,
+        goal_pos: Position,
+        speed: int,
+        acc: int,
+        is_relative: bool,
+        do_wait: bool,
+        timeout_ms: int,
+    ):
+        """Move from start_pos to goal_pos in a straight line."""
+
+        cmd = Command(
+            x=goal_pos.x,
+            y=goal_pos.y,
+            z=goal_pos.z,
+            roll=goal_pos.roll,
+            pitch=goal_pos.pitch,
+            yaw=goal_pos.yaw,
+            is_radian=goal_pos.is_radian,
+            speed=speed,
+            acc=acc,
+            is_cartesian=True,
+            is_relative=is_relative,
+            do_wait=do_wait,
+            timeout_ms=timeout_ms,
+        )
+
+        self.publish_command(cmd)
--- src/libalfred/wrapper/mixins/redis_user.py
+++ src/libalfred/wrapper/mixins/redis_user.py
@@ -7,8 +7,13 @@ class RedisUserMixin:
     """Mixin for redis capability."""
 
     rc: redis.Redis
+    pubsub: redis.client.PubSub
+    connected: bool = False
 
     def connect_redis(
         self, host: str, port: int, password: Optional[str] = None,
     ):
         self.rc = redis.Redis(host=host, port=port, password=password)
+        self.pubsub = self.rc.pubsub(ignore_subscribe_messages=True)
+
+        self.connected = True
